f623de729d99c65c6ea986c4323e6f0aeafd5834
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-11-25 16:40:11 +0100

    Styling added to page-title component

--- components/pageTitle.js
+++ components/pageTitle.js
@@ -1,6 +1,23 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+h1 {font-family: "Fredoka One", cursive;}
+.heading-container {  display: flex;
+    justify-content: center;}
+</style>
+<div class="heading-container">
+<h1></h1>
+</div>
+
+`;
+
 class PageTitle extends HTMLElement {
   constructor() {
     super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h1").innerText = this.getAttribute("title");
   }
 }
 
